6e8b7424a261333841655e9d586ff650a8b2717d
ming <chenming0317@gmail.com>
2023-03-21 17:10:03 -0700

    updated 03/21/2023

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const path = require('path')
+const fs = require('fs');
 const PORT = 3000;
 
 const app = express()
@@ -10,12 +11,26 @@ app.use(bodyParser())
 app.use(cors())
 app.use(express.static('public'))
 
+
 app.get("/", (req, res) => {
     res.render('index', { name: "MING", pic: "pic/tiaowu.gif" })
 })
 
+app.get("/jquery", (req, res) => {
+    fs.readFile('../JQueery/DOM/dom.html', (err, data)=>{
+        if(err){
+            releaseEvents.writeHead(404, {'Content-Type':'text/html'});
+            return res.end("404 Not Found");
+        }
+    })
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write(data);
+    res.end();
+})
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
 
 app.listen(PORT, function () {
     console.log("Start server in port " + PORT);
